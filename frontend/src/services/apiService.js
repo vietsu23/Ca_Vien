d43834e21a339b18ebc817bf6b19d92ce16f49c5
vietsu23 <nguyenvovietsu@gmail.com>
2025-11-26 11:38:51 +0700

    URL

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",   // backend URL
+  baseURL: "https://cavien-backend.onrender.com/api",   // backend URL
   timeout: 10000,
 });
 
